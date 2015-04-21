6af4bbfbea88200ad7d3bec966843c1bcc04d4ba
Adam Portier <ajportier@gmail.com>
2015-04-21 22:40:51 +0000

    Added code for sender service

---
+++ rspi/sender.c
@@ -0,0 +1,114 @@
+/*
+PORTIONS ADAPTED FROM SUNFOUNDER LABS CODE EXAMPLE
+http://www.sunfounder.com/
+
+1. sudo apt-get install libcurl4-openssl-dev
+2. sudo apt-get install libconfig-dev
+3. git clone git://git.drogon.net/wiringPi
+4. cd wiringPi
+5. git pull origin
+6. cd wiringPi
+7. ./build
+8. gcc sender.c -lwiringPi -lcurl -lconfig -o sender
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+#include <libconfig.h>
+#include <curl/curl.h>
+
+typedef unsigned char uchar;
+typedef unsigned int  uint;
+
+#define     ADC_CS    0
+#define     ADC_CLK   1
+#define     ADC_DIO   2
+
+uchar get_ADC_Result(void)
+{
+	//10:CH0
+	//11:CH1
+	uchar i;
+	uchar dat1=0, dat2=0;
+
+	digitalWrite(ADC_CS, 0);
+
+	digitalWrite(ADC_CLK,0);
+	digitalWrite(ADC_DIO,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);
+
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2); //CH0 10
+	digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);
+
+	digitalWrite(ADC_DIO,0);	delayMicroseconds(2); //CH0 0
+	
+	digitalWrite(ADC_CLK,1);	
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
+	digitalWrite(ADC_CLK,0);	
+	digitalWrite(ADC_DIO,1);    delayMicroseconds(2);
+	
+	for(i=0;i<8;i++)
+	{
+		digitalWrite(ADC_CLK,1);	delayMicroseconds(2);
+		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
+
+		pinMode(ADC_DIO, INPUT);
+		dat1=dat1<<1 | digitalRead(ADC_DIO);
+	}
+	
+	for(i=0;i<8;i++)
+	{
+		dat2 = dat2 | ((uchar)(digitalRead(ADC_DIO))<<i);
+		digitalWrite(ADC_CLK,1); 	delayMicroseconds(2);
+		digitalWrite(ADC_CLK,0);    delayMicroseconds(2);
+	}
+
+	digitalWrite(ADC_CS,1);
+
+	pinMode(ADC_DIO, OUTPUT);
+	
+	return(dat1==dat2) ? dat1 : 0;
+}
+
+int main(void)
+{
+	uchar tmp;
+    config_t cfg;
+    config_setting_t *setting;
+    const char *uri, *key;
+
+    config_init(&cfg);
+
+    if(! config_read_file(&cfg, "config.cfg")){
+        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
+            config_error_line(&cfg), config_error_text(&cfg));
+            config_destroy(&cfg);
+            return(EXIT_FAILURE);
+    }
+
+    if(config_lookup_string(&cfg, "uri", &uri)){
+        printf("URI: %s\n", uri);
+    } else {
+        fprintf(stderr, "No 'uri' setting in configuration\n");
+    }
+
+	if(wiringPiSetup() == -1){
+		printf("setup wiringPi failed !");
+		return 1; 
+	}
+
+	pinMode(ADC_CS,  OUTPUT);
+	pinMode(ADC_CLK, OUTPUT);
+
+	while(1){
+		pinMode(ADC_DIO, OUTPUT);
+		tmp = get_ADC_Result();
+		printf("%d\n",tmp);
+        delay(1000);
+	}
+
+	return 0;
+}
