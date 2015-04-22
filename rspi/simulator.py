fb703b2799e11af4374d563ca151d6e81c9cf7ae
Adam Portier <ajportier@gmail.com>
2015-04-22 14:14:27 -0400

    Made the simulator data more interesting

--- rspi/simulator.py
+++ rspi/simulator.py
@@ -35,6 +35,15 @@ if __name__ == '__main__':
     
     args, remaining_argv = parser.parse_known_args()
     sensor_id = ''
+    
+    current_action_seconds = 0
+    current_action_increase = False
+    current_action_value = 0
+    start_value = 100
+    min_value = 80
+    max_value = 120
+    current_value = 0
+    
     try:
         f = open(args.config, 'r')
         sensor_id = f.read()
@@ -47,7 +56,31 @@ if __name__ == '__main__':
             
     while True:
         try:
-            send_value(random.randint(1,10))
+            if current_value == 0:
+                current_value = start_value
+            
+            if current_action_seconds == 0:
+                # determine if we are going up or down
+                current_action_increase = (random.randint(0,1) == 1)
+                
+                # determine over how long
+                current_action_seconds = random.randint(1,5)
+                
+                # determine how much
+                current_action_value = random.randint(1,10)
+                
+            if current_value <= min_value:
+                current_action_increase = True
+            if current_value >= max_value:
+                current_action_increase = False
+            
+            if current_action_increase:
+                current_value += int(current_action_value / current_action_seconds)
+            else:
+                current_value += -int(current_action_value / current_action_seconds)
+            
+            send_value(current_value)
+            current_action_seconds += -1
             sleep(1)
         except KeyboardInterrupt:
             exit()
