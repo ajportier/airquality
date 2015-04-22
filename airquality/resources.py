874dde97934ef6cca5ac0b99a164e9386a13bea5
Adam Portier <ajportier@gmail.com>
2015-04-22 15:31:28 -0400

    Fixed a bad assumption about region intervals

--- airquality/resources.py
+++ airquality/resources.py
@@ -12,6 +12,7 @@ basic_parser = reqparse.RequestParser()
 basic_parser.add_argument('sensor_id', type=str, required=True)
 
 
+''' Adds a gas reading to the database using POST data from a RESTful call '''
 class AddGasReading(restful.Resource):
     def post(self):
         parser = basic_parser.copy()
@@ -27,6 +28,7 @@ class AddGasReading(restful.Resource):
             return {'response':'forbidden'}, 403
 
 
+''' Gets all gas reading data points for a single node over the requested seconds '''
 class GetGasReading(restful.Resource):
     def get(self):
         parser = basic_parser.copy()
@@ -40,6 +42,10 @@ class GetGasReading(restful.Resource):
         return jsonify({'node':node.sensor_id,'readings':readings})
 
 
+''' Gets the average of all the data points in a region over the given interval, one value for
+    each second of the interval. Does not include node identifier in the response (since it's a grouping)
+    TODO: Figure out why this is coming back as localtime and not UTC
+'''
 class GetRegionReading(restful.Resource):
     def get(self):
         parser = reqparse.RequestParser()
@@ -52,18 +58,31 @@ class GetRegionReading(restful.Resource):
         region = args['region']
         region_nodes = SensorNode.objects(region=region)
         readings = []
+        
+        # Stepping down from the requested number of seconds, pull
+        # all values between the current number of seconds and the next second
         while current_interval > 0:
             val = 0
             current_timestamp = timestamp - timedelta(seconds=current_interval)
-            current_readings = SensorReading.objects(node__in=region_nodes, created__gt=current_timestamp).exclude('id')
+            next_timestamp = current_timestamp + timedelta(seconds=1)
+            
+            current_readings = SensorReading.objects(node__in=region_nodes,
+                created__gt=current_timestamp,
+                created__lte=next_timestamp).exclude('id')
+
+            # If any readings exist for this second, average them and generate a single value
             if len(current_readings) > 0:
                 for reading in current_readings:
                     val += reading.value
                 val = val / len(current_readings)
-                current_interval += -1
                 
                 # Generate a POSIX timestamp of this averaged data set with millisecond precision
                 posix_time = int(time.mktime(current_timestamp.timetuple())*1000 + current_timestamp.microsecond/1000)
+                
+                # ...And add it to the return
                 readings.append({'created':{'$date':posix_time},'value':val})
+            
+            # Decrement the seconds interval we are looking at
+            current_interval += -1
 
         return jsonify({'region':region, 'readings':readings})
